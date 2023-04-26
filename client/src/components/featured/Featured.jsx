ce525b0fdd0d2d439dd93ce7db592ba1a4e4b1bd
machego <114348981+machego@users.noreply.github.com>
2023-04-26 19:30:52 +0200

    update

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
-import './Featured.scss'
+import React, { useState } from "react";
+import "./Featured.scss";
+import { useNavigate } from "react-router-dom";
 
-const Featured = () => {
+function Featured() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(`/gigs?search=${input}`);
+  };
   return (
-      <div className='featured'>
-          <div className='container'>
-              <div className='left'>
-                  <h1>Find the perfect <span>freelance</span> services for your business</h1>
-                  <div className='search'>
-                      <div className='searchInput'>
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h1>
+            Find the perfect <span>freelance</span> services for your business
+          </h1>
+          <div className="search">
+            <div className="searchInput">
                           <img src='../../../public/img/search.png' alt='' />
-                          <input type='text' placeholder='Try "building mobil app"' />
-                      </div>
-                      <button>Search</button>
-                  </div> 
-                  <div className='popular'>
-                      <span>Popular: </span>
-                      <button>Web Design</button>
-                      <button>Wordpress</button>
-                      <button>Logo Design</button>
-                      <button>AI Services</button>
-                  </div>    
-              </div>
+                          <input
+                type="text"
+                placeholder='Try "building mobil app"'
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </div>
+            <button onClick={handleSubmit}>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Web Design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
+        </div>
               <div className='right'>
                   <img src='../../../public/img/homepage/men00.png'/>
               </div>
--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -1,16 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GigCard from '../../components/gigCard/GigCard'
-import { gigs } from '../../data'
+import { useQuery } from '@tanstack/react-query'
 import './Gigs.scss'
+import newRequest from '../../utils/newRequest.js'
+import {useLocation} from 'react-router-dom'
 
 const Gigs = () => {
-  const [open, setOpen] = useState(false)
-  const [sort, setSort] = useState('sales')
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
+  const minRef = useRef();
+  const maxRef = useRef();
+
+  const { search } = useLocation();
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["gigs"],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
+  console.log(data)
 
   const reSort = (type) => {
     setSort(type)
     setOpen(false)
-  }
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [sort]);
+
+  const apply = () => {
+    refetch();
+  };
 
   return (
     <div className='gigs'>
@@ -21,31 +49,36 @@ const Gigs = () => {
         Explore the boundaries of art and technology with Fiverr`s AI artists 
         </p> 
         <div className='menu'>
-          <div className='left'>
-            <span>Budged</span>
-            <input type='text' placeholder='min' />
-            <input type='text' placeholder='max' />
-            <button>Apply</button> 
-          </div> 
-          <div className='right'>
-            <span className='sortby'>SortBy</span> 
-            <span className='sortType'>{ sort === 'sales' ? 'Best Selling' : 'Newest'}</span> 
+          <div className="left">
+            <span>Budget</span>
+            <input ref={minRef} type="number" placeholder="min" />
+            <input ref={maxRef} type="number" placeholder="max" />
+            <button onClick={apply}>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort by</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span> 
             <img src='/../../../public/img/down.png' alt='' onClick={() => setOpen(!open)} />
-            { open && (
-              <div className='rightMenu'>
-              {sort === 'sales' ? (
-              <span onClick={()=> reSort('createdAt')}>Newest</span>
-              ) : (
-                  <span onClick={ () => reSort('sales') }>Best Selling</span>
-                ) }
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
               </div>
-            ) }
-          </div> 
+            )}
+          </div>
         </div> 
-        <div className='cards'>
-          { gigs.map(gig => (
-            <GigCard key={gig.id} item={gig} />
-          ))}
+        <div className="cards">
+          {isLoading
+            ? "loading"
+            : error
+            ? "Something went wrong!"
+            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
         </div> 
       </div>
     </div>
